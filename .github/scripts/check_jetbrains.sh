19062a755ecfa55e1a1664985d2a2a1172a15e8d
Blake LaFleur <blake.lafleur@borrowworks.com>
2023-11-20 13:28:11 -0600

    Add actions and issue templates for version checks (#2)

---
+++ .github/scripts/check_jetbrains.sh
@@ -0,0 +1,24 @@
+# Checks the Jetbrains API for the latest version of a given product code and compares it to a given ebuild directory
+#
+
+# Known product codes:
+# IDEA: IIU
+# Rider: RD
+# Datagrip: DG
+
+
+CURRENT_TIMESTAMP=$(date +%s%N | cut -b1-13)
+LATEST_VERSION=$(curl -s "https://data.services.jetbrains.com/products?code=$1%2CIIC&release.type=release&_=${CURRENT_TIMESTAMP}" |  \
+    jq .[0].releases.[0].downloads.linux.link | \
+    grep -Eow '([0-9\.]+)' \
+)
+
+CURRENT_EBUILD=$(find $2 -name '*.ebuild' | sort -Vr | head -n1 | perl -n -e'/([0-9\.]+).ebuild/ && print $1')
+
+#echo "Current ebuild version: $CURRENT_EBUILD"
+#echo "Latest JetBrains version: $LATEST_VERSION"
+
+if [ "$LATEST_VERSION" != "$CURRENT_EBUILD" ]; then
+    echo "$LATEST_VERSION"
+    exit 1
+fi
